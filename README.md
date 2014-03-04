4e3c3afa232f8248fdade9011322312b27c1a967
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2014-03-05 00:13:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blog for Patrycja Krawiec realized with Jekyll and Bootstrap.
---
+++ _src/js/hover.zoom.js
@@ -0,0 +1,73 @@
+(function($){
+    $.fn.extend({ 
+        hoverZoom: function(settings) {
+ 
+            var defaults = {
+                overlay: true,
+                overlayColor: '#1abc9c',
+                overlayOpacity: 0.9,
+                zoom: 0,
+                speed: 300
+            };
+             
+            settings = $.extend(defaults, settings);
+         
+            return this.each(function() {
+            
+                var s = settings;
+                var hz = $(this);
+                var image = $('img', hz);
+                    
+                if(s.overlay === true) {
+                    $(image).parent()
+                    .append('<div class="zoomOverlay"><div style="vertical-align: middle;margin-top: 35%;"><h4>'+
+                        $(image).parent().data("title") +
+                        '</h4><hr><p>'+
+                        $(image).parent().data("city")+
+                        '</p></div></div>');
+                    $(image).parent().find('.zoomOverlay').css({
+                        opacity:0, 
+                        display: 'block', 
+                        backgroundColor: s.overlayColor
+                    }); 
+                }
+            
+                var width = $(image).width();
+                var height = $(image).height();
+            
+                $(image).fadeIn(1000, function() {
+                    $(image).parent().css('background-image', 'none');
+                    hz.hover(function() {
+                        $('img', image).stop().animate({
+                            height: height + s.zoom,
+                            marginLeft: -(s.zoom),
+                            marginTop: -(s.zoom)
+                        }, s.speed);
+                        if(s.overlay === true) {
+                            $(image).parent().find('.zoomOverlay').stop().animate({
+                                opacity: s.overlayOpacity
+                            }, s.speed);
+                        }
+                    }, function() {
+                        $('img', image).stop().animate({
+                            height: height,
+                            marginLeft: 0,
+                            marginTop: 0
+                        }, s.speed);
+                        if(s.overlay === true) {
+                            $(image).parent().find('.zoomOverlay').stop().animate({
+                                opacity: 0
+                            }, s.speed);
+                        }
+                    });
+                });
+            });
+        }
+    });
+    $(document).ready( function () {
+        $('.green').hoverZoom({
+            overlayColor: '#1abc9c',
+            zoom: 0
+        });
+    });
+})(jQuery);
